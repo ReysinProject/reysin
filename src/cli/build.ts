016ab9482b67b5a4ef3e794833b2f2babf47f809
l.guerra--rago <l.guerra--rago@xefi.fr>
2024-10-02 16:59:40 +0200

    fucking shitted language

---
+++ src/cli/build.ts
@@ -0,0 +1,18 @@
+import { build } from "vite";
+import {loadConfig} from "../utils/config-loader";
+
+export async function buildApp() {
+	const config = loadConfig();
+
+	await build({
+		configFile: false,
+		root: process.cwd(),
+		base: config.vite.base,
+		build: {
+			outDir: config.vite.outDir,
+			assetsDir: config.vite.assetsDir,
+		},
+	});
+
+	console.log("Build complete");
+}
